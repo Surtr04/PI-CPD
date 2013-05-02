cefd5e7adc3feba043a7e8e9062da5ceca9a498e
Rui Brito <ruibrito666@gmail.com>
2013-05-02 18:19:45 +0100

    removed incompatibilites with search

--- AMN/room_assignment/src/generators.c
+++ AMN/room_assignment/src/generators.c
@@ -1,5 +1,7 @@
 #include "generators.h"
 
+#ifdef __APPLE__
+
 float randFloat() {
 	float f = (float) arc4random() / (float) UINT32_MAX;
 	return f;
@@ -19,3 +21,33 @@ unsigned long randomul_limited (unsigned long min ,unsigned long max) {
 	float f = randFloat();
 	return f * ( max - min + 1 ) + min;
 }
+
+
+
+#else
+
+
+
+float randFloat() {
+	float f = (float) rand() / (float) UINT32_MAX;
+	return f;
+}
+
+double randDouble() {
+
+	union _32to64 r;
+	r.u32[0] = rand();	
+	r.u32[1] = rand();	
+
+	return ((double) r.u64 / (double) UINT64_MAX);
+}
+
+
+unsigned long randomul_limited (unsigned long min ,unsigned long max) {
+	float f = randFloat();
+	return f * ( max - min + 1 ) + min;
+}
+
+
+
+#endif
--- AMN/room_assignment/src/generators.h
+++ AMN/room_assignment/src/generators.h
@@ -1,18 +1,13 @@
 #pragma once
 #include <stdint.h>
 #include <limits.h>
-
-#ifdef __APPLE__
 #include <stdlib.h>
-#else
-#include <bsd/stdlib.h>
-#endif
+
 
 union _32to64 {
 		uint32_t u32[2];
 		uint64_t u64;
 };
-
 
 float randFloat();
 double randDouble();
