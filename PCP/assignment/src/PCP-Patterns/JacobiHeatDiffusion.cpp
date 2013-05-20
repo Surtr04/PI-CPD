395ae244ef5cfb0234de1581ad586ae553fa8d63
Rui Brito <ruibrito666@gmail.com>
2013-05-20 10:03:37 +0100

    o cilk não vale nada

---
+++ PCP/assignment/src/PCP-Patterns/JacobiHeatDiffusion.cpp
@@ -0,0 +1,60 @@
+﻿
+#include <stdio.h>
+#include <stdlib.h>
+#include  <omp.h>
+
+#define size 512  // testar também 1024 ou 2048 (não deve escalar)
+#define iter 2000
+
+double domain[size][size];
+double aux[size][size];
+
+inline void stencil(double in[size][size], double out[size][size], int i, int j) {
+    out[i][j]= 0.25 * (in[i-1][j]+in[i+1][j]+in[i][j-1]+in[i][j+1]);    
+}
+
+void jacobi(double a[size][size]){
+    
+    #pragma omp parallel num_threads(4)
+    for(int k=0; k<iter; k++) {
+    
+//#pragma omp parallel for num_threads(4)  /* less efficient */
+        #pragma omp  for
+        for(int i=1; i<size-1; i++) {
+            for(int j=1; j<size-1; j++) {
+                stencil(a,aux,i,j);
+            }
+        }
+        
+//#pragma omp parallel for num_threads(4) /* less efficient */
+        #pragma omp  for
+        for(int i=1; i<size-1; i++) {
+            for(int j=1; j<size; j++) {
+                stencil(aux,a,i,j);
+            }
+        }
+        
+    }
+
+}
+
+double MFlops(double time) {
+    
+    return(4.0*(size-2.0)*(size-2.0)*iter/(time*1000000.0));
+}
+
+int main(int argc, char * argv[]) {
+    
+    
+    
+    double starttime = omp_get_wtime();
+    
+    jacobi(domain);
+    
+    double walltime = omp_get_wtime()-starttime;
+    
+    printf("F=%0.1f Time=%.0.2f; %.0f MFlop/s\n",domain[10][10],walltime, MFlops(walltime) );
+    
+    return 0;
+}
+
--- PCP/assignment/src/fib.cpp
+++ PCP/assignment/src/fib.cpp
@@ -4,19 +4,29 @@
 #include <cilk/cilk.h>
 #include <cilk/cilk_api.h>
 
-int fib(int n)
-{
+double seq_fib(double n) {
+    double i, j;
+    if (n<2) return(n);
+    
+    return(seq_fib(n-2)+seq_fib(n-1));
+}    
+
+double fib(double n) {
     if (n < 2)
         return n;
-    int x = cilk_spawn fib(n-1);
-    int y = fib(n-2);
+
+    if (n < 30)
+        return seq_fib(n);
+
+    double x = cilk_spawn fib(n-1);
+    double y = fib(n-2);
     cilk_sync;
     return x + y;
 }
 
 int main(int argc, char *argv[]) {
     
-    int n = 45;
+    double n = 47;
 
     // If we've got a parameter, assume it's the number of workers to be used
     if (argc > 1)
@@ -34,12 +44,12 @@ int main(int argc, char *argv[]) {
 
     // Time how long it takes to calculate the nth Fibonacci number
     clock_t start = clock();
-    int result = fib(n);
+    double result = fib(n);
     clock_t end = clock();
 
     // Display our results
     double duration = (double)(end - start) / CLOCKS_PER_SEC;
-    printf("Fibonacci number #%d is %d.\n", n, result);
+    printf("Fibonacci number #%f is %.0f.\n", n, result);
     printf("Calculated in %.3f seconds using %d workers.\n",
            duration, __cilkrts_get_nworkers());
 
