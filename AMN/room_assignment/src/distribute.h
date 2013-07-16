8531512b9124e4f0eef113e6aec74911068a2037
Rui Brito <ruibrito666@gmail.com>
2013-07-16 17:17:03 -0500

    took care of the stupid warnings

--- AMN/room_assignment/src/distribute.h
+++ AMN/room_assignment/src/distribute.h
@@ -4,3 +4,5 @@ void randomseed (unsigned int seed);
 void rand_dislikes (unsigned long* dislike_matrix, unsigned long students, unsigned long min, unsigned long max);
 unsigned long distribute_random (unsigned long * dislikes, unsigned long * rooms, unsigned long * assigned, unsigned long nstudents);
 unsigned long distribute_wo_sa (unsigned long * dislikes, unsigned long * rooms, unsigned long nstudents);
+unsigned long distribute_w_sa (unsigned long * dislikes, unsigned long * rooms, unsigned long nstudents, double t0);
+
